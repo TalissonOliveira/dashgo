4360dd47ba84e4600c727e60b52492fe7c0accb4
Talisson Oliveira <talissonlux@outlook.com>
2022-06-04 17:16:41 -0300

    feat: creates the login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
 import type { NextPage } from 'next'
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-const Home: NextPage = () => {
+const SignIn: NextPage = () => {
   return (
-    <h1>Dashgo</h1>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input label="E-mail" name="email" type="email" />
+
+          <Input label="Senha" name="password" type="password" />
+        </Stack>
+
+        <Button type="submit" mt={6} colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
 
-export default Home
+export default SignIn
