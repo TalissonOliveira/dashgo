ccb8602d43961b88920cc03acf8e74460235773b
Talisson Oliveira <talissonlux@outlook.com>
2022-06-07 01:33:32 -0300

    feat: creates the header component

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import { Header } from '../components/Header'
+
+const Dashboard: NextPage = () => {
+  return (
+    <Header />
+  )
+}
+
+export default Dashboard
