3b668337ac5ec0358be7a674e7855c7c0f8c62fc
Talisson Oliveira <talissonlux@outlook.com>
2022-06-08 20:21:19 -0300

    feat: creates the create new user page

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,43 @@
+import type { NextPage } from 'next'
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from '@chakra-ui/react'
+import { Header } from '../../components/Header'
+import { Sidebar } from '../../components/Sidebar'
+import { Input } from '../../components/Form/Input'
+
+const CreateUser: NextPage = () => {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" maxW={1480} my="6" mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" bg="gray.800" p="8" borderRadius={8}>
+          <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
+
+          <Divider my="6" borderColor="gray.700" />
+
+          <VStack>
+            <SimpleGrid w="100%" minChildWidth="240px" spacing="8">
+              <Input name="name" label="Nome completo" />
+              <Input name="email" label="E-mail" />
+            </SimpleGrid>
+            <SimpleGrid w="100%" minChildWidth="240px" spacing="8">
+              <Input name="password" type="password" label="Senha" />
+              <Input name="password_confirmation" type="password" label="Confirmação da senha" />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Button colorScheme="pink">Salvar</Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
+
+export default CreateUser
