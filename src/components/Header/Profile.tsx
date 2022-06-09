b98c4cdcf3826d9382f73939dc7acaa81850a964
Talisson Oliveira <talissonlux@outlook.com>
2022-06-09 17:36:23 -0300

    feat: makes the header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,21 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Jane Doe</Text>
-        <Text color="gray.300" fontSize="small">
-          janedoe@mail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Jane Doe</Text>
+          <Text color="gray.300" fontSize="small">
+            janedoe@mail.com
+          </Text>
+        </Box>
+      )}
+
       <Avatar
         size="md"
         name="Jane Doe"
