3cf8a2fe029ffd4025608af30f50c1604bef7c5d
Talisson Oliveira <talissonlux@outlook.com>
2022-06-09 21:17:10 -0300

    feat: adjustments in the create user form

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -12,17 +12,17 @@ const CreateUser: NextPage = () => {
       <Flex w="100%" maxW={1480} my="6" mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" bg="gray.800" p="8" borderRadius={8}>
+        <Box flex="1" bg="gray.800" p={["6", "8"]} borderRadius={8}>
           <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
           <Divider my="6" borderColor="gray.700" />
 
           <VStack>
-            <SimpleGrid w="100%" minChildWidth="240px" spacing="8">
+            <SimpleGrid w="100%" minChildWidth="240px" spacing={["6", "8"]}>
               <Input name="name" label="Nome completo" />
               <Input name="email" label="E-mail" />
             </SimpleGrid>
-            <SimpleGrid w="100%" minChildWidth="240px" spacing="8">
+            <SimpleGrid w="100%" minChildWidth="240px" spacing={["6", "8"]}>
               <Input name="password" type="password" label="Senha" />
               <Input name="password_confirmation" type="password" label="Confirmação da senha" />
             </SimpleGrid>
